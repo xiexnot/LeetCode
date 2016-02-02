e4f30c8f6ed8e7940eea8898a4788930a79482ca
xiexnot <xiexnot@gmail.com>
2016-02-02 20:56:25 +0800

    delete the old file

--- 1-100/LeetCode_1.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def twoSum(self, num, target):
-        tmp = {}
-        for i in range(len(num)):
-            if target - num[i] in tmp:
-                return(tmp[target - num[i]] +1, i + 1)
-            else:
-                tmp[num[i]] = i;
