6812b8f1884c4456bd6d95d1f4e8d0d39aac08a5
xiexnot <xiexnot@gmail.com>
2016-02-11 22:13:49 +0800

    Updated on 2016.02.11

--- 1-100/LeetCode 060 Permutation Sequence.py
+++ 1-100/LeetCode 060 Permutation Sequence.py
@@ -6,5 +6,33 @@ class Solution(object):
 		:type k: int
 		:rtype: str
 		"""
+		Factorial = []
+		nums = []
+		chosen = []
+		for i in range(n):
+			chosen.append(False)
+			nums.append(-1)
+		for i in range(n):
+			if i == 0:
+				Factorial.append(1)
+			else:
+				Factorial.append(Factorial[Factorial.__len__()-1]*i)
+		return self.kthPermutation(Factorial, nums, chosen, n, k)
 		
+	def kthPermutation(self, Factorial, nums, chosen, n, k):
+		Result = ""
+		k = k-1
+		for i in range(n)[::-1]:
+			nums[i] = k / Factorial[i]
+			k = k % Factorial[i]
+		print nums
+		for i in range(nums.__len__())[::-1]:
+			key = nums[i]
+			for j in range(chosen.__len__()):
+				if chosen[j] == False:
+					key -= 1
+				if key == -1:
+					chosen[j] = True
+					Result += str(j+1)
+					break
 		return Result
